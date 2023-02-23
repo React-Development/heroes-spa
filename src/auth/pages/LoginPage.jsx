4882f8b9fd62edda36dde1f05b72956d709715fe
Brayan Garcia <bgarcia.95@outlook.com>
2023-02-23 15:37:23 -0600

    add ui and navigation to heroes router

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,3 +1,22 @@
+import { useNavigate } from "react-router-dom";
+
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const navigate = useNavigate();
+
+  const onLogin = () => {
+    navigate("/", {
+      replace: true,
+    });
+  };
+
+  return (
+    <div className="container mt-5">
+      <h3>Login</h3>
+      <hr />
+
+      <button className="btn btn-primary" onClick={onLogin}>
+        Login
+      </button>
+    </div>
+  );
 };
